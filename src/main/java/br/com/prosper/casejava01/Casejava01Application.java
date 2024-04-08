6f83dce1f2d52b6ade08bbd3fa1cd26cbab52b56
CorreaBrunoMiguel <devsistemascorrea@gmail.com>
2024-04-08 13:59:56 -0300

    Commit inicial

---
+++ src/main/java/br/com/prosper/casejava01/Casejava01Application.java
@@ -0,0 +1,13 @@
+package br.com.prosper.casejava01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Casejava01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Casejava01Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/prosper/casejava01/Casejava01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prosper.casejava01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Casejava01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
